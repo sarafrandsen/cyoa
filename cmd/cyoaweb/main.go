9c299d88def02408cdb82a8ca55ad9602ef9401c
Sara Frandsen <sara.frandsen@gmail.com>
2018-02-12 16:29:51 -0800

    will run basic cyoa story in web browser

---
+++ cmd/cyoaweb/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/sarafrandsen/cyoa"
+)
+
+func main() {
+	port := flag.Int("port", 3000, "The port to start the CYOA web app on")
+	filename := flag.String("file", "gopher.json", "the JSON file with the CYOA story")
+	flag.Parse()
+	fmt.Printf("Using the story in %s.\n", *filename)
+
+	f, err := os.Open(*filename)
+	if err != nil {
+		panic(err)
+	}
+
+	story, err := cyoa.JsonStory(f)
+	if err != nil {
+		panic(err)
+	}
+
+	h := cyoa.NewHandler(story)
+	fmt.Printf("Starting the server at: %d\n", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
+}
